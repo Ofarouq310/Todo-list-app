a1237ff1ce2d21a11327f9581f525e8e17856317
Omar Farouq <ofarouq310@gmail.com>
2022-11-05 23:04:33 +0200

    add src input files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 import './style.css';
-import addTask from './modules/addtask.js';
+import addTask from './modules/addDeleteTask.js';
+import tasks from './modules/task-obj.js';
 
-// Call function to display the list of tasks
-addTask();
+// Declare function to add tasks to the task-object
+const addItem = document.getElementById('add_task');
+addItem.addEventListener('keypress', () => {
+  if (window.event.keyCode === 13) {
+    if (addItem.value === '') {
+      const task = {
+        index: tasks.length + 1,
+        description: '"Do Task"',
+        completed: false,
+      };
+      tasks.push(task);
+      addTask(task);
+    } else {
+      const task = {
+        index: tasks.length + 1,
+        description: addItem.value,
+        completed: false,
+      };
+      tasks.push(task);
+      addTask(task);
+    }
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+});
+
+window.addEventListener('click', (event) => {
+  if (!event.target.matches('.dots_icon')) {
+    const dotDiv = document.getElementsByClassName('dot_div');
+    for (let i = 0; i < dotDiv.length; i += 1) {
+      dotDiv[i].style.display = 'none';
+    }
+  }
+});
+
+window.addEventListener('load', () => {
+  tasks.forEach((task) => {
+    addTask(task);
+  });
+});
