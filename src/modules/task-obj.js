e299fa2ab1b2bfaa976bb25d9997f1bb74888b75
Omar Farouq <ofarouq310@gmail.com>
2022-11-12 01:36:22 +0200

    add modify function

--- src/modules/task-obj.js
+++ src/modules/task-obj.js
@@ -1,3 +1,10 @@
 // Declare array of objects
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-export default tasks;
+/* eslint-disable-next-line */
+let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+// Declare function to modify tasks object if needed
+function modifyTasks(modTasks) {
+  tasks = modTasks;
+}
+
+export { tasks, modifyTasks };
